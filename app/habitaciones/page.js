02f323ffc417d94e7b66f8fe32219d6ceff582c9
Juliana Arroyave Arango <julianaarroyave301009@correo.itm.edu.co>
2026-03-26 21:13:01 -0500

    Arreglo desastre

--- app/habitaciones/page.js
+++ app/habitaciones/page.js
@@ -4,8 +4,6 @@ import HabitacionesModule from "../../components/habitaciones/HabitacionesModule
 
 export default function HabitacionesPage() {
   return (
-    <div style={{ padding: "20px" }}>
-      <HabitacionesModule />
-    </div>
+    <HabitacionesModule />
   );
 }
